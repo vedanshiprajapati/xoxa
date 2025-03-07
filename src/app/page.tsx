2ae96c82646aa3572178b3be624717450520bc93
Vedanshi555 <umesha.ap71@gmail.com>
2025-03-07 23:49:55 +0530

    removed previous chatdashboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,21 @@ import ChatDashboard from "@/components/chat/chatDashboard";
 export default function Home() {
   const router = useRouter();
 
-  // useEffect(() => {
-  //   // Check if user is authenticated
-  //   const checkAuth = async () => {
-  //     const {
-  //       data: { session },
-  //     } = await supabase.auth.getSession();
+  useEffect(() => {
+    // Check if user is authenticated
+    const checkAuth = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
 
-  //     if (!session) {
-  //       // If not authenticated, redirect to login
-  //       router.push("/login");
-  //     }
-  //   };
+      if (!session) {
+        // If not authenticated, redirect to login
+        router.push("/login");
+      }
+    };
 
-  //   checkAuth();
-  // }, [router]);
+    checkAuth();
+  }, [router]);
 
   return (
     <main>
